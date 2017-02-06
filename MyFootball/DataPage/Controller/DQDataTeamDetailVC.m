d6e15cf1adb13493a256364a26fe628f16851ab1
git <873965585@qq.com>
2017-02-06 17:54:54 +0800

    assist shower

--- MyFootball/DataPage/Controller/DQDataTeamDetailVC.m
+++ MyFootball/DataPage/Controller/DQDataTeamDetailVC.m
@@ -6,21 +6,111 @@
 //  Copyright © 2016年 huangjinyang. All rights reserved.
 //
 
+#define KHeaderHeight 200.0
 #import "DQDataTeamDetailVC.h"
 //#import "UINavigationBar+Awesome.h"
 
 @interface DQDataTeamDetailVC ()<UITableViewDelegate,UITableViewDataSource,UIScrollViewDelegate>
+{
+    UIView *_header;
+    UIImageView *_headerImageView;
+    UIView *_lineView;
+    UIStatusBarStyle _statStyle;
+    UITextField* t;
+}
 @property(nonatomic,strong)UITableView* tableView;
 @property (strong, nonatomic) UIView *header;
 @property (strong, nonatomic) UIImageView *headerImageView;
 @end
-
+NSString *const cellId = @"cellID";
 @implementation DQDataTeamDetailVC
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self.navigationController setNavigationBarHidden:YES];
+}
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
+}
+- (void)preparaTopHeadr
+{
+    _header = [[UIView alloc]initWithFrame:CGRectMake(0, 0,UIScreenWidth, KHeaderHeight)];
+    _header.backgroundColor =[MyTools colorWithHexString:@"0xF8F8F8"];
+    
+    [self.view addSubview:_header];
+    
+    _headerImageView = [[UIImageView alloc]initWithFrame:_header.bounds];
+    _headerImageView.backgroundColor = [MyTools colorWithHexString:@"0x000033"];
+    _headerImageView.contentMode = UIViewContentModeScaleAspectFill;
+    _headerImageView.clipsToBounds = YES;//图片过大的裁切
+    [_header addSubview:_headerImageView];
+    //设置图像
+    NSURL *url = [NSURL URLWithString:@"http://c.hiphotos.baidu.com/zhidao/pic/item/5ab5c9ea15ce36d3c704f35538f33a87e950b156.jpg"];
+    [_headerImageView sd_setImageWithURL:url placeholderImage:IMAGENAME(@"2016")];
+    //添加分割线一个像素
+    CGFloat height = 1/[UIScreen mainScreen].scale;
+    _lineView = [[UIView alloc]initWithFrame:CGRectMake(0, KHeaderHeight, UIScreenWidth, height)];
+    _lineView.backgroundColor = [UIColor lightGrayColor];
+    [_header addSubview:_lineView];
+}
+
+- (void)prepareTableView
+{
+    UITableView *tableView = [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    [tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellId];
+    
+    [self.view addSubview:tableView];
+    //设置标哥的间距
+    tableView.contentInset = UIEdgeInsetsMake(200, 0, 0, 0);
+    tableView.scrollIndicatorInsets = UIEdgeInsetsMake(200, 0, 0, 0);
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self initUI];
-    self.view.backgroundColor=[UIColor whiteColor];
+    t=[[UITextField alloc]init];
+    [self.view addSubview:t];
+    [t mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(self.view);
+        make.size.mas_equalTo(CGSizeMake(200, 50));
+    }];
+    t.backgroundColor=[UIColor grayColor];
+    [t addObserver:self forKeyPath:@"text" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:nil];
+    
+    UITextField* t2=[[UITextField alloc]init];
+    [self.view addSubview:t2];
+    [t2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view).offset(100);
+        make.centerX.equalTo(self.view);
+        make.size.mas_equalTo(CGSizeMake(200, 50));
+    }];
+    t2.backgroundColor=[UIColor grayColor];
+//    [self initUI];
+//    self.automaticallyAdjustsScrollViewInsets = NO;
+//    self.view.backgroundColor = [UIColor whiteColor];
+    
+//    [self prepareTableView];
+    
+//    [self preparaTopHeadr];
+    
+//    _statStyle = UIStatusBarStyleLightContent;
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if([keyPath isEqualToString:@"isEditing"]) {
+        // text has changed
+        DQLog(@"123");
+        dispatch_queue_t t=dispatch_get_main_queue();
+    }
+}
+
+-(void)dealloc{
+    [t removeObserver:self forKeyPath:@"isFirstResponder"];
 }
 
 -(void)initUI{
@@ -29,32 +119,13 @@
     self.tableView.delegate=self;
     self.tableView.dataSource=self;
     [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.view).offset(-64);
+        make.top.equalTo(self.view);
         make.left.right.bottom.equalTo(self.view);
     }];
-    
-    self.header = [[UIView alloc] initWithFrame:CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y, self.view.frame.size.width, 107)];
-    [self.view addSubview:self.header];
-    
-    self.headerImageView = [[UIImageView alloc] initWithFrame:self.header.frame];
-    self.headerImageView.image = IMAGENAME(@"bg");
-    self.headerImageView.contentMode = UIViewContentModeScaleToFill;
-    self.headerImageView.layer.masksToBounds=YES;
-    self.header.clipsToBounds = YES;
-    [self.header addSubview:self.headerImageView];
-    
-    self.tableView.tableHeaderView= [[UIView alloc] initWithFrame:CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y, self.view.frame.size.width, self.header.frame.size.height + 13)];
-}
+    //设置标哥的间距
+    self.tableView.contentInset = UIEdgeInsetsMake(200, 0, 0, 0);
+    self.tableView.scrollIndicatorInsets = UIEdgeInsetsMake(200, 0, 0, 0);
 
--(void)viewWillAppear:(BOOL)animated{
-    [super viewWillAppear:animated];
-    [self.navigationController.navigationBar setShadowImage:[UIImage new]];
-    [self.navigationController.navigationBar lt_setBackgroundColor:[UIColor clearColor]];
-}
--(void)viewWillDisappear:(BOOL)animated{
-    [super viewWillDisappear:animated];
-    [self.navigationController.navigationBar lt_reset];
-    
 }
 
 #pragma mark - UITableViewDelegate
@@ -72,40 +143,60 @@
     cell.textLabel.text=[NSString stringWithFormat:@"%zd",indexPath.row];
     return cell;
 }
-
-#pragma mark - UIScrollViewDelegate
--(void)scrollViewDidScroll:(UIScrollView *)scrollView{
-    CGFloat offsetY = scrollView.contentOffset.y;
-    CGFloat alpha = 0;
-    
-    CATransform3D headerTransform = CATransform3DIdentity;
-    
-    // 下拉
-    if (offsetY < 0) {
-        CGFloat headerScaleFactor = -(offsetY) / self.header.bounds.size.height;
-        CGFloat headerSizevariation = ((self.header.bounds.size.height * (1.0 + headerScaleFactor)) - self.header.bounds.size.height)/2.0;
-        headerTransform = CATransform3DTranslate(headerTransform, 0, headerSizevariation, 0);
-        headerTransform = CATransform3DScale(headerTransform, 1.0 + headerScaleFactor, 1.0 + headerScaleFactor, 0);
-        self.header.layer.transform = headerTransform;
-        self.header.hidden=NO;
-    }
-    // 上拉
-    else {
-        self.header.hidden=YES;
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    CGFloat offsety = scrollView.contentOffset.y+scrollView.contentInset.top;
+    if (offsety <= 0)
+    {
+        //放大
+        CGRect frame= _header.frame;
+        frame.origin.y = 0;
+        
+        frame.size.height = KHeaderHeight-offsety;
+        _header.frame = frame;
+        
+        frame = _headerImageView.frame;
+        frame.size.height = _header.height;;
+        _headerImageView.frame = frame;;
+        
+        
+        
+        //设置透明度
+        _headerImageView.alpha = 1.0;
+        //根据透明度修改状态栏颜色
+        _statStyle = UIStatusBarStyleLightContent;
+        //主动更新状态栏
+        [self.navigationController setNeedsStatusBarAppearanceUpdate];
         
     }
-
-    
-    if (offsetY >= 64) {
-        alpha=((offsetY-64)/64 <= 1.0 ? (offsetY-64)/64:1);
-        [self.navigationController.navigationBar lt_setBackgroundColor:[ThemeColor colorWithAlphaComponent:alpha]];
-        self.title=@"球队";
+    else
+    {
+        //整体移动
         
-    }else{
-        [self.navigationController.navigationBar lt_setBackgroundColor:[UIColor clearColor]];
-        self.title=@"";
+        CGRect frame= _header.frame;
+        //header的最小y值
+        CGFloat min = KHeaderHeight-64;
+        frame.origin.y = -MIN(min, offsety);
+        
+        frame.size.height = KHeaderHeight;
+        _header.frame = frame;
+        
+        frame = _headerImageView.frame;
+        frame.size.height = _header.height;;
+        _headerImageView.frame = frame;;
+        //设置透明度
+        CGFloat progress = 1-(offsety/min);
+        _headerImageView.alpha = progress;
+        NSLog(@"%f",offsety/min);
+        //根据透明度修改状态栏颜色
+        _statStyle = (progress<0.5)?UIStatusBarStyleDefault:UIStatusBarStyleLightContent;
+        //主动更新状态栏
+        [self.navigationController setNeedsStatusBarAppearanceUpdate];
+        
     }
     
+    CGRect frame = _lineView.frame;
+    frame.origin.y = _header.height-_lineView.height;
+    _lineView.frame = frame;
 }
-
 @end
--- MyFootball/HomePage/Controller/DQShipViewController.m
+++ MyFootball/HomePage/Controller/DQShipViewController.m
@@ -14,23 +14,172 @@
 
 @implementation DQShipViewController
 
+
+{
+    UIWindow* _window;
+    UIButton* _button;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.view.backgroundColor=[UIColor whiteColor];
-    UILabel* l1=[UILabel new];
-    l1.text=@"懂球号";
+//    UILabel* l1=[UILabel new];
+//    l1.text=@"懂球号";
     
-    [self.view addSubview:l1];
-    l1.textAlignment=1;
-    [l1 mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.height.mas_equalTo(30);
-        make.width.mas_equalTo(100);
-        make.centerY.mas_equalTo(self.view.mas_centerY);
-        make.centerX.mas_equalTo(self.view.mas_centerX);
-    }];
+//    [self.view addSubview:l1];
+//    l1.textAlignment=1;
+//    [l1 mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.height.mas_equalTo(30);
+//        make.width.mas_equalTo(100);
+//        make.centerY.mas_equalTo(self.view.mas_centerY);
+//        make.centerX.mas_equalTo(self.view.mas_centerX);
+//    }];
+    
+    [self performSelector:@selector(createButton) withObject:nil afterDelay:0];
     
     // Do any additional setup after loading the view.
+}
+
+
+#pragma mark - 创建悬浮的按钮
+
+- (void)createButton{
+    
+    _window = [UIApplication sharedApplication].windows[0];
+    
+    _button = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    [_button setTitle:@"按钮" forState:UIControlStateNormal];
+    
+    _button.frame = CGRectMake(UIScreenWidth - 70, UIScreenHeight - 150, 60, 60);
+    
+    _button.titleLabel.font = [UIFont systemFontOfSize:13.0f];
+    
+    [_button setBackgroundColor:[UIColor orangeColor]];
+    
+    _button.layer.cornerRadius = 30;
+    
+    _button.layer.masksToBounds = YES;
+    
+    [_button addTarget:self action:@selector(resignButton) forControlEvents:UIControlEventTouchUpInside];
+    
+    //    _window = [[UIWindow alloc]initWithFrame:CGRectMake(kSize_width - 70, kSize_height - 150, 50, 50)];
+    
+    //    _window.windowLevel = UIWindowLevelAlert+1;
+    
+    //    _window.backgroundColor = [UIColor greenColor];
+    
+    //    _window.layer.cornerRadius = 25;
+    
+    //    _window.layer.masksToBounds = YES;
+    
+    [_window addSubview:_button];
+    
+    //    [_window makeKeyAndVisible];//显示window
+    
+    //放一个拖动手势，用来改变控件的位置
+    
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(changePostion:)];
+    
+    [_button addGestureRecognizer:pan];
+    
+}
+
+- (void)resignButton{
+    
+}
+
+//手势事件 －－ 改变位置
+
+-(void)changePostion:(UIPanGestureRecognizer *)pan{
+    
+    CGPoint point = [pan translationInView:_button];
+    
+    CGFloat width = [UIScreen mainScreen].bounds.size.width;
+    
+    CGFloat height = [UIScreen mainScreen].bounds.size.height;
+    
+    CGRect originalFrame = _button.frame;
+    
+    if (originalFrame.origin.x >= 0 && originalFrame.origin.x+originalFrame.size.width <= width) {
+        
+        originalFrame.origin.x += point.x;
+        
+    }if (originalFrame.origin.y >= 64 && originalFrame.origin.y+originalFrame.size.height <= height) {
+        
+        originalFrame.origin.y += point.y;
+        
+    }
+    
+    _button.frame = originalFrame;
+    
+    [pan setTranslation:CGPointZero inView:_button];
+    
+    if (pan.state == UIGestureRecognizerStateBegan) {
+        
+        _button.enabled = NO;
+        
+    }else if (pan.state == UIGestureRecognizerStateChanged){
+        
+    } else {
+        
+        CGRect frame = _button.frame;
+        
+        //是否越界
+        
+        BOOL isOver = NO;
+        
+        if (frame.origin.x < 0) {
+            
+            frame.origin.x = 0;
+            
+            isOver = YES;
+            
+        } else if (frame.origin.x+frame.size.width > width) {
+            
+            frame.origin.x = width - frame.size.width;
+            
+            isOver = YES;
+            
+        }if (frame.origin.y < 64) {
+            
+            frame.origin.y = 64;
+            
+            isOver = YES;
+            
+        } else if (frame.origin.y+frame.size.height > height) {
+            
+            frame.origin.y = height - frame.size.height;
+            
+            isOver = YES;
+            
+        }if (isOver) {
+            
+            [UIView animateWithDuration:0.3 animations:^{
+                
+                _button.frame = frame;
+                
+            }];
+            
+        }
+        
+        if(_button.x+_button.width/2>UIScreenWidth/2){
+            frame.origin.x=UIScreenWidth-_button.width;
+        }
+        else{
+            frame.origin.x=0;
+        }
+        [UIView animateWithDuration:0.3 animations:^{
+            
+            _button.frame = frame;
+            
+        }];
+
+        _button.enabled = YES;
+        
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
